a27206980ae042ad873e5576065e2bdebbf897d0
Алина <alinamalaya09092006@icloud.com>
2024-12-22 14:37:03 +0500

    Add files via upload

---
+++ метод_итераций.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+#include <ctime>
+using namespace std;
+
+float fi(float x)
+{
+    return 1 / (3 + sin(3.6 * x));
+}
+void func(float x, float (*fi)(float))
+{
+    cout << "Введите значение епсилон: ";
+    float eps;
+    cin >> eps;
+    int start = clock();
+    while (abs((fi(x) - x)) > eps)
+    {
+        cout << "xn = " << x << ", xn + 1 = " << fi(x) << endl;
+        x = fi(x);
+    }
+    cout << "Корень уравнения = " << x << endl;
+    int end = clock();
+    cout << "Время выполнения программы: " << end - start;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x = 0.5;
+    func(x, fi);
+    return 0;
+}
---
+++ метод_ньютона.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+using namespace std;
+
+float fi(float x)
+{
+	return x - 1 / (3 + sin(3.6 * x));
+}
+float pr(float x)
+{
+	return 1 - (3.6 * cos(3.6 * x) / pow(3 + sin(3.6 * x), 2));
+}
+void func(float x, float (*fi)(float), float (*pr)(float))
+{ 
+	cout << "Введите значение епсилон:" << endl;
+	float eps;
+	cin >> eps;
+
+	while (abs((x - (fi(x) / pr(x)) - x)) > eps)
+	{
+		cout << "xn = " << x << ", xn + 1 = " << x - (fi(x) / pr(x)) << endl;
+		x = x - (fi(x) / pr(x));
+	}
+	cout << "Корень уравнения = " << x;
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x = 0.5;
+	func(x, fi, pr);
+}
